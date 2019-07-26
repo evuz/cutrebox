6d6b6491f51f9eb8807ce962546a451302482fdd
evuz <jesus_jgb_45@hotmail.com>
2019-07-26 17:59:34 +0200

    feat: Cutrebox add Back and Spine

--- src/components/Cutrebox/Cutrebox.jsx
+++ src/components/Cutrebox/Cutrebox.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 import Front from '../Front';
 
 import './Cutrebox.scss';
+import Spine from '../Spine/Spine';
+import Back from '../Back';
 import Strip from '../Strip';
+import Flap from '../Flap';
 
 function Cutrebox() {
   return (
     <div className="cutrebox">
       <div className="cutrebox__content">
-        <Strip top />
+        <Back />
+        <Spine title="Superman de Brian M. Bendis" vol={3} />
         <Front />
+        <Strip height={614} top/>
       </div>
     </div>
   );
--- src/components/Strip/Strip.jsx
+++ src/components/Strip/Strip.jsx
@@ -4,17 +4,29 @@ import './Strip.scss';
 import mixinsClassnames from '../../utils/mixinsClassnames';
 import Flap from '../Flap';
 
-function Strip({ horizontal, top, bottom, left, right }) {
+function Strip({
+  horizontal,
+  top,
+  bottom,
+  left,
+  right,
+  children,
+  height,
+  width
+}) {
   const classNames = mixinsClassnames({
     strip: true,
     'strip--horizontal': horizontal
   });
+  const style = { height, width };
   return (
     <div className="col">
       {top ? <Flap /> : null}
       <div className="row fill">
         {left ? <Flap /> : null}
-        <div className={classNames} />
+        <div style={style} className={classNames}>
+          {children}
+        </div>
         {right ? <Flap /> : null}
       </div>
       {bottom ? <Flap /> : null}
