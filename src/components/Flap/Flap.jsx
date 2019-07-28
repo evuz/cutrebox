7fc9b2585cc8f16745169c1ee21527a32fff612c
Jesus Gomez Bellido <jesus_jgb_45@hotmail.com>
2019-07-28 16:25:27 +0200

    feat: flap component adjust by position

--- src/components/Flap/Flap.jsx
+++ src/components/Flap/Flap.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
 import './Flap.scss';
+import mixinsClassnames from '../../utils/mixinsClassnames';
 
-function Flap() {
+const positionFlap = {
+  top: {},
+  bottom: { invert: true },
+  left: { vertical: true },
+  right: { vertical: true, invert: true }
+};
+
+function Flap({ className: propsClassNames, position = 'top' }) {
+  const positionValues = positionFlap[position];
+  const classNames = mixinsClassnames({
+    flap: true,
+    'flap--vertical': positionValues.vertical,
+    'flap--invert': positionValues.invert,
+    [propsClassNames]: propsClassNames
+  });
   return (
-    <div className="flap">
+    <div className={classNames}>
       <div className="flap__triangle flap__triangle--left" />
       <div className="flap__content" />
       <div className="flap__triangle flap__triangle--right" />
