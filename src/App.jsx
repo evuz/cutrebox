ecff51bbf9f86eff2b446b437cd58fbac477791e
evuz <jesus_jgb_45@hotmail.com>
2019-08-20 08:36:27 +0200

    feat: print cutrebox with correct size!

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Cutrebox from './components/Cutrebox/Cutrebox';
 import Float from './components/Float/Float';
 
 function print() {
-  html2canvas(document.body).then(canvas => {
+  html2canvas(document.querySelector('#cutrebox'), {scale: 3}).then(canvas => {
     document.body.appendChild(canvas);
-    // const img = canvas.toDataURL('image/png');
-    // const doc = new jsPDF({ format: 'a3', orientation: 'landscape' });
-    // doc.addImage(img, 'PNG', 10, 10);
-    // doc.save();
+    const img = canvas.toDataURL('image/png');
+    const doc = new jsPDF({ format: 'a3', orientation: 'landscape' });
+    doc.addImage(img, 'PNG', 0, 0, 420, 297);
+    doc.save('cutrebox');
   });
 }
 
